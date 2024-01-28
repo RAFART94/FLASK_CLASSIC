66060e8b3193207d01b43640055a56c809e5565a
RAFART94 <danielgrs94@gmail.com>
2024-01-28 16:22:25 +0000

    Últimos cambios

--- registros_ig/routes.py
+++ registros_ig/routes.py
@@ -1,5 +1,10 @@
 from registros_ig import app
+from flask import render_template
 
 @app.route('/')
-def hello():
-    return 'HHola esto es flask classic'
+def index():
+    diccionario = [{'Fecha':'2024-01-01', 'Concepto':'Compra de reyes', 'Monto':-100},
+                   {'Fecha':'2024-01-02', 'Concepto':'Compra de comida', 'Monto':-200},
+                   {'Fecha':'2024-01-03', 'Concepto':'Compra de bebida', 'Monto':-50},
+                   {'Fecha':'2024-01-04', 'Concepto':'Compra de roscon', 'Monto':-20}]
+    return render_template('index.html', variable=diccionario)
