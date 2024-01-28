de4d0fe94d467646765642c7193d58788161f572
RAFART94 <danielgrs94@gmail.com>
2024-01-28 17:44:40 +0000

    Actualización arquitectura flask

---
+++ registros_ig/models.py
@@ -0,0 +1,7 @@
+def select_all():
+     diccionario = [{'date':'2024-01-01', 'concept':'Compra de reyes', 'quantity':-100},
+                   {'date':'2024-01-02', 'concept':'Compra de comida', 'quantity':-200},
+                   {'date':'2024-01-03', 'concept':'Compra de bebida', 'quantity':-50},
+                   {'date':'2024-01-04', 'concept':'Compra de roscon', 'quantity':-20}]
+     
+     return diccionario
--- registros_ig/routes.py
+++ registros_ig/routes.py
@@ -1,10 +1,8 @@
 from registros_ig import app
 from flask import render_template
+from registros_ig.models import *
 
 @app.route('/')
 def index():
-    diccionario = [{'Fecha':'2024-01-01', 'Concepto':'Compra de reyes', 'Monto':-100},
-                   {'Fecha':'2024-01-02', 'Concepto':'Compra de comida', 'Monto':-200},
-                   {'Fecha':'2024-01-03', 'Concepto':'Compra de bebida', 'Monto':-50},
-                   {'Fecha':'2024-01-04', 'Concepto':'Compra de roscon', 'Monto':-20}]
-    return render_template('index.html', variable=diccionario)
+    dic = select_all()
+    return render_template('index.html', datos=dic)
